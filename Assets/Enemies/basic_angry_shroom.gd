7fea2f92df228d95834249d8573c03a41207519d
B3lgiumWaffle <131527171+B3lgiumWaffle@users.noreply.github.com>
2024-03-17 18:47:51 +0200

    Added money drop
    Enemies can drop money now

--- Assets/Enemies/basic_angry_shroom.gd
+++ Assets/Enemies/basic_angry_shroom.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 @onready var ledgeCheckLeft: = $LedgeCheckLeft
 @onready var ledgeCheckRight: = $LedgeCheckRight
-
+var loots = preload("res://Assets/Collectables/Diamond.tscn")
 #Variables
 var health = 500
 var speed = 175 #Change this if need some tweaking
@@ -56,3 +56,39 @@ func take_damage(damage):
 	health -= damage
 	if health < 0:
 		queue_free()
+		spawn_loot()
+
+
+#Spawns loot
+func spawn_loot():
+	var random_ammount = randf_range(2,4)
+	var loot_radius = 20
+	for i in range(random_ammount):
+		var loot_instance = loots.instantiate()
+		loot_instance.position = find_empty_position(loot_radius)
+		get_tree().get_root().add_child(loot_instance)
+
+
+
+
+#Handles spawning logic, but there is a problem with spawning inside walls
+func find_empty_position(radius):
+	var position = Vector2.ZERO
+	var tries = 0
+	while tries < 10:
+		position.x = global_position.x + randf_range(-radius, radius)
+		position.y = global_position.y + randf_range(-radius, radius)
+		
+		var overlapping = false
+		for child in get_tree().get_root().get_children():
+			if child != self and child.is_in_group("Collectible"):
+				var distance_squared = child.global_position.distance_squared_to(position)
+				if distance_squared < radius * radius:
+					overlapping = true
+					break
+			
+		if not overlapping:
+			return position
+		
+		tries += 1
+	return position
--- Assets/Enemies/tower_mushroom.gd
+++ Assets/Enemies/tower_mushroom.gd
@@ -3,6 +3,7 @@ extends Node2D
 @onready var rayCast = $RayCast2D
 @onready var timer = $ReloadTimer
 @export var ammo: PackedScene
+var loots = preload("res://Assets/Collectables/Diamond.tscn")
 
 var health = 100
 var player 
@@ -48,4 +49,36 @@ func _shoot():
 func take_damage(damage):
 	health -= damage
 	if health < 0:
+		spawn_loot()
 		queue_free()
+
+#Spawns loot 
+func spawn_loot():
+	var random_ammount = randf_range(1,2)
+	var loot_radius = 10 #Change this value for some enemies
+	for i in range(random_ammount):
+		var loot_instance = loots.instantiate()
+		loot_instance.position = find_empty_position(loot_radius)
+		get_tree().get_root().add_child(loot_instance)
+
+
+#Handles spawning logic, but there is a problem with spawning inside walls
+func find_empty_position(radius):
+	var position = Vector2.ZERO
+	var tries = 0
+	while tries < 10:
+		position.x = global_position.x + randf_range(-radius, radius)
+		position.y = global_position.y + randf_range(-radius, radius)
+		
+		var overlapping = false
+		for child in get_tree().get_root().get_children():
+			if child != self and child.is_in_group("Collectible"):
+				var distance_squared = child.global_position.distance_squared_to(position)
+				if distance_squared < radius * radius:
+					overlapping = true
+					break
+		if not overlapping:
+			return position
+		
+		tries += 1
+	return position
