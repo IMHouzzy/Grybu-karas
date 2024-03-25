5b57873f30083e31b36c579145832ef8a8be1520
IMHouzzy <162693863+IMHouzzy@users.noreply.github.com>
2024-03-26 00:44:34 +0200

    Revert "Added Health cappacity power up"
    This reverts commit bdf438513137095d8b98d291cd122d6ff8dec973.

--- Global.gd
+++ Global.gd
@@ -4,7 +4,6 @@ var currency: int
 var maxHealth = 3 #Global max health
 var currentHealth = maxHealth #Global current Health
 var increasedHealth: bool = false
-var increasedCapacity: bool = false
 
 #If coin collected add 1
 func collectedCurrency():
