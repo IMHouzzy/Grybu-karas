37cd703d81775bdf542fce6fe1ced78484cdbaa7
IMHouzzy <162693863+IMHouzzy@users.noreply.github.com>
2024-04-20 13:44:37 +0300

    fixed

--- Assets/level/next_level_trigger.gd
+++ Assets/level/next_level_trigger.gd
@@ -6,6 +6,6 @@ func _on_body_entered(body):
 	if body.is_in_group("player"):
 		var current_scene_file = get_tree().current_scene.scene_file_path
 		var next_level_number = current_scene_file.to_int()+1
-		var next_level_path = FILE_BEGIN + str(next_level_number)+".tscn"
+		var next_level_path = FILE_BEGIN + str(next_level_number)+"_.tscn"
 		Transition.chnage_scene(next_level_path)
 		print(current_scene_file)
