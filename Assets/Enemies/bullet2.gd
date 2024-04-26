0ea0cb5f01cc3f1819860e9bf8015c2af27eb71d
B3lgiumWaffle <131527171+B3lgiumWaffle@users.noreply.github.com>
2024-04-26 16:07:25 +0300

    Added lady mushroom to the game (new mob)

---
+++ Assets/Enemies/bullet2.gd
@@ -0,0 +1,22 @@
+extends Area2D
+
+#var RIGHT = Vector2.RIGHT
+var direction : Vector2 = Vector2.RIGHT
+var speed = 10
+
+# Called when the node enters the scene tree for the first time.
+func _physics_process(delta):
+	position += direction * speed
+
+func destroy():
+	queue_free()
+
+
+#When exits the screen
+func _on_visible_on_screen_enabler_2d_screen_exited():
+	queue_free()
+
+
+func _on_body_entered(body):
+	if body.is_in_group("player"):
+		destroy()
